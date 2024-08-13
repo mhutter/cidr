d4703ee8d293e6a0fad2d1d9692d5a0ac83deb78
Manuel Hutter <manuel@hutter.io>
2024-08-13 16:21:27 +0200

    chore: Remove deprecated tooling, clean up pipeline
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell."${system}" = pkgs.mkShell {
+        packages = with pkgs; [
+          go-tools
+        ];
+      };
+    };
+}
